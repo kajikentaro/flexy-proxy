1facf934a4b127dffde6adfd94e48a55ecf10fca
kajikentaro <kajindowsxp@gmail.com>
2024-08-22 23:55:13 +0900

    --wip-- [skip ci]

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.4
 
 require (
 	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/phsym/console-slog v0.3.1
 	github.com/stretchr/testify v1.9.0
 	gopkg.in/yaml.v3 v3.0.1
--- routers/handle_template.go
+++ routers/handle_template.go
@@ -1,8 +1,11 @@
 package routers
 
 import (
+	"bytes"
 	"net/http"
+	"os/exec"
 
+	"github.com/google/shlex"
 	"github.com/kajikentaro/elastic-proxy/models"
 )
 
@@ -22,21 +25,61 @@ type HandleTemplate struct {
 	headers     map[string]string
 }
 
-func (h *HandleTemplate) Handle(w http.ResponseWriter, r *http.Request) {
-	h.handler.Handle(w, r)
+// customResponseWriterは、http.ResponseWriterをラップして書き込まれたデータをキャプチャする
+type customResponseWriter struct {
+	http.ResponseWriter
+	body bytes.Buffer
+}
 
+func (w *customResponseWriter) Write(b []byte) (int, error) {
+	return w.body.Write(b)
+}
+
+func (h *HandleTemplate) Handle(w http.ResponseWriter, r *http.Request) {
+	// customResponseWriterを使って、書き込まれたデータをキャプチャ
+	crw := &customResponseWriter{ResponseWriter: w}
+
+	// handlerの処理を実行し、その出力をキャプチャ
+	h.handler.Handle(crw, r)
+
+	INPUT := "sed -E 's/hoge/fuga/g'"
+	cmdArgs, err := shlex.Split(INPUT)
+	if err != nil {
+		http.Error(w, "Failed to execute command", http.StatusInternalServerError)
+		return
+	}
+
+	// コマンドを実行して標準入力にキャプチャしたボディを渡し、結果を取得
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	cmd.Stdin = &crw.body
+
+	// コマンドの実行結果を取得
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		http.Error(w, "Failed to execute command", http.StatusInternalServerError)
+		return
+	}
+
+	// コンテンツタイプが指定されている場合、レスポンスヘッダーに設定
 	if h.contentType != "" {
-		// only if the contentType is specified, overwrite
 		w.Header().Set("Content-Type", h.contentType)
 	}
 
+	// ステータスコードが指定されている場合、レスポンスのステータスコードを設定
 	if h.statusCode != 0 {
-		// only if the statusCode is specified, overwrite
 		w.WriteHeader(h.statusCode)
 	}
 
+	// その他のヘッダーを設定
 	for v, k := range h.headers {
 		w.Header().Set(v, k)
+	}
+
+	// コマンドの結果をレスポンスとして書き込む
+	_, err = w.Write(out.Bytes())
+	if err != nil {
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
 	}
 }
 
