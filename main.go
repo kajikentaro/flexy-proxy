7d477e68fcf9c1d333c0a04febcabd4d73d6b8a9
kajikentaro <kajindowsxp@gmail.com>
2024-10-05 23:52:17 +0900

    add --version flag

--- main.go
+++ main.go
@@ -15,12 +15,30 @@ func fatalf(format string, a ...any) {
 	os.Exit(1)
 }
 
+// this will be specified like:
+// go build -ldflags "-X 'main.version=1.0.0'"
+var version string
+
+func getVersion() string {
+	if version == "" {
+		return "unknown"
+	}
+	return version
+}
+
 func main() {
 	var customConfigPath string
 	flag.StringVar(&customConfigPath, "f", utils.DEFAULT_CONFIG_PATH, "Path to custom config file")
 	var portNum int
 	flag.IntVar(&portNum, "p", 8888, "Port number")
+	var showVersion bool
+	flag.BoolVar(&showVersion, "version", false, "Show version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println(getVersion())
+		return
+	}
 
 	config, err := utils.ReadConfigYaml(customConfigPath)
 	if err != nil {
