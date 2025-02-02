2a42eb6efaff6dcf2457f08b16bcd9a92056b25b
kajikentaro <kajindowsxp@gmail.com>
2025-02-02 19:00:27 +0900

    add a subcommand, 'test'

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 
+	"github.com/kajikentaro/flexy-proxy/models"
 	"github.com/kajikentaro/flexy-proxy/proxy"
 	"github.com/kajikentaro/flexy-proxy/utils"
 	"github.com/spf13/cobra"
@@ -21,12 +24,7 @@ func getVersion() string {
 	return version
 }
 
-var (
-	customConfigPath string
-	portNum          int
-)
-
-func startProxy() {
+func startProxy(customConfigPath string, portNum int) {
 	proxyConfig, err := utils.ParseConfig(customConfigPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -39,12 +37,46 @@ func startProxy() {
 	fmt.Fprintf(os.Stderr, "%v\n", http.ListenAndServe(addr, proxy))
 }
 
+func testRoute(customConfigPath string, testUrl string) error {
+	proxyConfig, err := utils.ParseConfig(customConfigPath)
+	if err != nil {
+		return err
+	}
+
+	parsedUrl, err := url.Parse(testUrl)
+	if err != nil {
+		return err
+	}
+
+	handler, matchedUrl, err := proxyConfig.Router.GetHandler(parsedUrl)
+
+	if errors.Is(err, models.ErrRouteNotFound) {
+		return fmt.Errorf("route not found")
+	}
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("URL:", matchedUrl)
+	fmt.Println("Type:", handler.GetType())
+	fmt.Println("Info:")
+	for key, val := range handler.GetResponseInfo() {
+		fmt.Printf("    %s: %s\n", key, val)
+	}
+	return nil
+}
+
 func main() {
+	var (
+		customConfigPath string
+		portNum          int
+	)
+
 	rootCmd := &cobra.Command{
 		Use:   "main",
 		Short: "A YAML-based flexible proxy for software development",
 		Run: func(cmd *cobra.Command, args []string) {
-			startProxy()
+			startProxy(customConfigPath, portNum)
 		},
 	}
 
@@ -63,10 +95,22 @@ func main() {
 		Use:   "start",
 		Short: "Start the proxy server",
 		Run: func(cmd *cobra.Command, args []string) {
-			startProxy()
+			startProxy(customConfigPath, portNum)
 		},
 	}
 
-	rootCmd.AddCommand(versionCmd, startCmd)
+	testCmd := &cobra.Command{
+		Use:   "test [URL]",
+		Short: "Print the configured routing information for the given URL",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := testRoute(customConfigPath, args[0]); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	rootCmd.AddCommand(versionCmd, startCmd, testCmd)
 	rootCmd.Execute()
 }
