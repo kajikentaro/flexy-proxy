e2c5843c38d2f0c5e9ef0e210703e0ab743e19ca
kajikentaro <58505538+kajikentaro@users.noreply.github.com>
2025-04-13 08:00:05 +0000

    refactor, rename: sse_test

--- integration_test/sse_test/sse_test.go
+++ integration_test/sse_test/sse_test.go
@@ -4,10 +4,10 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
@@ -15,6 +15,7 @@ import (
 	"github.com/kajikentaro/flexy-proxy/loggers"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var PROXY_PORT_NUMBER = 8091
@@ -42,71 +43,63 @@ func TestMain(m *testing.M) {
 	// setup proxy server
 	{
 		ctx, cancel := context.WithCancel(context.Background())
-		test_utils.StartProxyServer(ctx, PROXY_HTTP_ADDRESS, "sse_test.yaml")
+		err := test_utils.StartProxyServer(ctx, PROXY_HTTP_ADDRESS, "sse_test.yaml")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to start a proxy server:", err)
+			os.Exit(1)
+		}
 		defer cancel()
 	}
 	m.Run()
 }
 
 func TestSSEWithoutProxy(t *testing.T) {
-	start := time.Now()
 	res, err := http.Get(SAMPLE_SERVER_URL + "/sse")
 	assert.NoError(t, err)
 	defer res.Body.Close()
 
-	scanner := bufio.NewScanner(res.Body)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		duration := time.Since(start)
-		assert.Less(t, duration, SSE_TIMEOUT_DURATION)
-		assert.True(t, strings.Contains(line, "data: SSE message") || line == "", "unexpected line: %s", line)
-
-		start = time.Now()
-	}
-	assert.NoError(t, scanner.Err())
+	testSSEStreams(t, res.Body)
 }
 
 // test SSE communication with proxy but without rewrite
 func TestSSEWithProxyWithoutRewrite(t *testing.T) {
-
-	start := time.Now()
 	res, err := test_utils.Request(PROXY_URL, "http://localhost:8092/sse")
 	assert.NoError(t, err)
 	defer res.Body.Close()
 
-	scanner := bufio.NewScanner(res.Body)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		duration := time.Since(start)
-		assert.Less(t, duration, SSE_TIMEOUT_DURATION)
-		assert.True(t, strings.Contains(line, "data: SSE message") || line == "", "unexpected line: %s", line)
-
-		start = time.Now()
-	}
-	assert.NoError(t, scanner.Err())
-
+	testSSEStreams(t, res.Body)
 }
 
 // test SSE communication with proxy and with rewrite
 func TestSSEWithProxy(t *testing.T) {
-	{
-		start := time.Now()
-		res, err := test_utils.Request(PROXY_URL, "http://sample-sse.test/sse")
-		assert.NoError(t, err)
-		defer res.Body.Close()
+	res, err := test_utils.Request(PROXY_URL, "http://sample-sse.test/sse")
+	assert.NoError(t, err)
+	defer res.Body.Close()
 
-		scanner := bufio.NewScanner(res.Body)
+	testSSEStreams(t, res.Body)
+}
+
+func testSSEStreams(t *testing.T, body io.ReadCloser) {
+	expectedOutput := []string{
+		"data: SSE message 0\n\n",
+		"data: SSE message 0\n\ndata: SSE message 1\n\n",
+		"data: SSE message 0\n\ndata: SSE message 1\n\ndata: SSE message 2\n\n",
+		"data: SSE message 0\n\ndata: SSE message 1\n\ndata: SSE message 2\n\ndata: SSE message 3\n\n",
+		"data: SSE message 0\n\ndata: SSE message 1\n\ndata: SSE message 2\n\ndata: SSE message 3\n\ndata: SSE message 4\n\n",
+	}
+
+	output := ""
+	go func() {
+		scanner := bufio.NewScanner(body)
 		for scanner.Scan() {
 			line := scanner.Text()
-
-			duration := time.Since(start)
-			assert.Less(t, duration, SSE_TIMEOUT_DURATION)
-			assert.True(t, strings.Contains(line, "data: SSE message") || line == "", "unexpected line: %s", line)
-
-			start = time.Now()
+			output += line + "\n"
 		}
-		assert.NoError(t, scanner.Err())
+	}()
+
+	time.Sleep(test_utils.SSE_DURATION / 2)
+	for i := 0; i < 5; i++ {
+		require.Equal(t, expectedOutput[i], output)
+		time.Sleep(test_utils.SSE_DURATION)
 	}
 }
